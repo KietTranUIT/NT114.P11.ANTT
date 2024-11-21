11dc88694bfc53b9dcf49c824668b04f4c9d9622
Tran Kiet <kiettranuit@gmail.com>
2024-11-21 10:58:26 +0700

    frontend: add product page and product list page

--- src/cms/app/src/App.js
+++ src/cms/app/src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
+import AddProduct from "./pages/product/AddProduct";
+import ListProducts from "./pages/product/ListProduct";
 import ProtectedRoute from "./routes/protect";
 import NotFound from "./pages/404";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/addproduct" exact element={<AddProduct/>}/>
+        <Route path="/listproducts" exact element={<ListProducts/>}/>
         <Route path="/auth" exact element={<Auth/>} />
         <Route element={ <ProtectedRoute />}>
           <Route path="/" element={ <Dashboard /> } />
--- src/cms/app/src/components/header/Header.js
+++ src/cms/app/src/components/header/Header.js
@@ -3,7 +3,7 @@ import "./header.css";
 function Header() {
     return (
         <>
-        <div className="navbar">
+        <div className="navbar-header">
                 <div className="navbar-content">
                 <div className="navbar-left">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu text-body fs-5">
@@ -21,7 +21,6 @@ function Header() {
                 </div>
                 </div>
         </div>
-        
         <div className="overwrite-header"></div>
         </>
     )
