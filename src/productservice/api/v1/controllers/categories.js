657c91737f0f2a73f01957571b7780d15c46a80b
Tran Kiet <kiettranuit@gmail.com>
2024-11-21 23:18:21 +0700

    feat: update a category

--- src/productservice/api/v1/controllers/categories.js
+++ src/productservice/api/v1/controllers/categories.js
@@ -1,8 +1,9 @@
 const errorCodes = require('./../../../config/errors');
 const ErrorObj = require('../models/errors');
 const Category = require('../models/categories');
-const { checkRequiredParameters } = require('../help');
+const { checkRequiredParameters, strongParameters } = require('../help');
 const slugify = require('slugify');
+const { where } = require('sequelize');
 
 // Get all record in categories table
 module.exports.getAll = async (req, res) => {
@@ -58,6 +59,42 @@ module.exports.create = async (req, res) => {
             data: category
         })
 
+    } catch (error) {
+        res.status(500).json(ErrorObj.createInternalError(error.message))
+    }
+}
+
+// Update a category
+module.exports.update = async (req, res) => {
+    try {
+        const categoryId = req.params.id
+        let data = strongParameters(req.body, ['name', 'slug', 'description', 'parentId'])
+
+        if (data.parentId) {
+            // Check if parent id is integer
+            if (!Number.isInteger(data.parentId) || data.parentId < 0) {
+                const err = new ErrorObj(errorCodes.invalidData, 422, "Invalid data", "parentId must be number and greater 0.", { pointer: "/parentId" })
+                return res.status(422).json({ errors: [err] })
+            }
+        }
+
+        let category
+        try {
+            category = await Category.update(data, {
+                where: { id: categoryId }
+            })
+        } catch (err_db) {
+            if (err_db.name === 'SequelizeUniqueConstraintError') {
+                if (err_db.errors[0].path === 'slug') {
+                    err = new ErrorObj(errorCodes.duplicateEntry, 422, 'Duplicate record', 'duplicate slug category.', { pointer: '/slug' })
+                } else {
+                    err = new ErrorObj(errorCodes.duplicateEntry, 422, 'Duplicate entry', 'duplicate name category.', { pointer: '/name' })
+                }
+                return res.status(422).json({ errors: [err] })
+            }
+            throw err_db
+        }
+        res.status(200).json({data})
     } catch (error) {
         res.status(500).json(ErrorObj.createInternalError(error.message))
     }
--- src/productservice/api/v1/routes/categories.js
+++ src/productservice/api/v1/routes/categories.js
@@ -5,6 +5,7 @@ const categories = require('./../controllers/categories');
 
 router.get('/', categories.getAll)
 router.post('/', categories.create)
+router.put('/:id', categories.update)
 
 module.exports = router;
 
